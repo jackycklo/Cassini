59e88dd85ec9c948c2d143f8484cbf070bf658b8
jackycklo <jackycklo2@gmail.com>
2017-05-01 18:16:25 +0800

    Initial Commit

---
+++ Cassini/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cassini
+//
+//  Created by Chi kit Lo on 1/5/2017.
+//  Copyright © 2017 Chi kit Lo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
